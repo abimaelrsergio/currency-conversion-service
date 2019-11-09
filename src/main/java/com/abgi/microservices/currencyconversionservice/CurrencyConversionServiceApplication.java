6a1f581c55261f0301793475aba1ba70b6b447e5
abimaelrsergio <abimaelr.sergio@gmail.com>
2019-11-08 23:59:07 -0300

    Atualizacao

--- src/main/java/com/abgi/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/abgi/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,10 @@ package com.abgi.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.abgi.microservices.currencyconversionservice")
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
